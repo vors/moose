e1bf2a9bf7c8c2e6552df613a6d43dd16308d73a
Sergei Vorobev <sergei.vorobev@getcruise.com>
2023-11-12 12:04:28 -0800

    rip-off rip

---
+++ src/main.rs
@@ -0,0 +1,199 @@
+use std::collections::HashMap;
+use std::io::Write;
+use std::str::FromStr;
+
+use clap::Parser;
+use itertools::Itertools;
+use miette::{Context, IntoDiagnostic};
+use tracing_subscriber::filter::Directive;
+use tracing_subscriber::{fmt, layer::SubscriberExt, util::SubscriberInitExt, EnvFilter};
+use url::Url;
+
+use rattler_installs_packages::tags::WheelTags;
+use rattler_installs_packages::{normalize_index_url, resolve, Pep508EnvMakers, Requirement};
+
+use indicatif::{MultiProgress, ProgressDrawTarget};
+use std::io;
+use std::sync::OnceLock;
+use tracing_subscriber::fmt::MakeWriter;
+
+/// Returns a global instance of [`indicatif::MultiProgress`].
+///
+/// Although you can always create an instance yourself any logging will interrupt pending
+/// progressbars. To fix this issue, logging has been configured in such a way to it will not
+/// interfere if you use the [`indicatif::MultiProgress`] returning by this function.
+pub fn global_multi_progress() -> MultiProgress {
+    static GLOBAL_MP: OnceLock<MultiProgress> = OnceLock::new();
+    GLOBAL_MP
+        .get_or_init(|| {
+            let mp = MultiProgress::new();
+            mp.set_draw_target(ProgressDrawTarget::stderr_with_hz(20));
+            mp
+        })
+        .clone()
+}
+
+#[derive(Clone)]
+pub struct IndicatifWriter {
+    progress_bars: MultiProgress,
+}
+
+impl IndicatifWriter {
+    pub fn new(pb: MultiProgress) -> Self {
+        Self { progress_bars: pb }
+    }
+}
+
+impl io::Write for IndicatifWriter {
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        self.progress_bars.suspend(|| io::stderr().write(buf))
+    }
+
+    fn flush(&mut self) -> io::Result<()> {
+        self.progress_bars.suspend(|| io::stderr().flush())
+    }
+}
+
+impl<'a> MakeWriter<'a> for IndicatifWriter {
+    type Writer = IndicatifWriter;
+
+    fn make_writer(&'a self) -> Self::Writer {
+        self.clone()
+    }
+}
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[clap(num_args=1.., required=true)]
+    specs: Vec<Requirement>,
+
+    /// Base URL of the Python Package Index (default <https://pypi.org/simple>). This should point
+    /// to a repository compliant with PEP 503 (the simple repository API).
+    #[clap(default_value = "https://pypi.org/simple/", long)]
+    index_url: Url,
+
+    #[clap(short)]
+    verbose: bool,
+}
+
+async fn actual_main() -> miette::Result<()> {
+    let args = Args::parse();
+
+    // Setup tracing subscriber
+    tracing_subscriber::registry()
+        .with(fmt::layer().with_writer(IndicatifWriter::new(global_multi_progress())))
+        .with(
+            EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| get_default_env_filter(args.verbose)),
+        )
+        .init();
+
+    // Determine cache directory
+    let cache_dir = dirs::cache_dir()
+        .ok_or_else(|| miette::miette!("failed to determine cache directory"))?
+        .join("rattler/pypi");
+    tracing::info!("cache directory: {}", cache_dir.display());
+
+    // Construct a package database
+    let package_db = rattler_installs_packages::PackageDb::new(
+        Default::default(),
+        &[normalize_index_url(args.index_url.clone())],
+        cache_dir,
+    )
+    .into_diagnostic()
+    .wrap_err_with(|| {
+        format!(
+            "failed to construct package database for index {}",
+            args.index_url
+        )
+    })?;
+
+    // Determine the environment markers for the current machine
+    let env_markers = Pep508EnvMakers::from_env()
+        .await
+        .into_diagnostic()
+        .wrap_err_with(|| {
+            "failed to determine environment markers for the current machine (could not run Python)"
+        })?;
+    tracing::debug!(
+        "extracted the following environment markers from the system python interpreter:\n{:#?}",
+        env_markers
+    );
+
+    let compatible_tags = WheelTags::from_env().await.into_diagnostic()?;
+    tracing::debug!(
+        "extracted the following compatible wheel tags from the system python interpreter: {}",
+        compatible_tags.tags().format(", ")
+    );
+
+    // Solve the environment
+    let blueprint = match resolve(
+        &package_db,
+        &args.specs,
+        &env_markers,
+        Some(&compatible_tags),
+        HashMap::default(),
+        HashMap::default(),
+    )
+    .await
+    {
+        Ok(blueprint) => blueprint,
+        Err(err) => miette::bail!("Could not solve for the requested requirements:\n{err}"),
+    };
+
+    // Output the selected versions
+    println!("{}:", console::style("Resolved environment").bold());
+    for spec in args.specs.iter() {
+        println!("- {}", spec);
+    }
+
+    println!();
+    let mut tabbed_stdout = tabwriter::TabWriter::new(std::io::stdout());
+    writeln!(
+        tabbed_stdout,
+        "{}\t{}",
+        console::style("Name").bold(),
+        console::style("Version").bold()
+    )
+    .into_diagnostic()?;
+    for pinned_package in blueprint.into_iter().sorted_by(|a, b| a.name.cmp(&b.name)) {
+        write!(tabbed_stdout, "{name}", name = pinned_package.name.as_str()).into_diagnostic()?;
+        if !pinned_package.extras.is_empty() {
+            write!(
+                tabbed_stdout,
+                "[{}]",
+                pinned_package.extras.iter().map(|e| e.as_str()).join(",")
+            )
+            .into_diagnostic()?;
+        }
+        writeln!(
+            tabbed_stdout,
+            "\t{version}",
+            version = pinned_package.version
+        )
+        .into_diagnostic()?;
+    }
+    tabbed_stdout.flush().into_diagnostic()?;
+
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() {
+    if let Err(e) = actual_main().await {
+        eprintln!("{e:?}");
+    }
+}
+
+/// Constructs a default [`EnvFilter`] that is used when the user did not specify a custom RUST_LOG.
+pub fn get_default_env_filter(verbose: bool) -> EnvFilter {
+    let mut result = EnvFilter::new("rip=info")
+        .add_directive(Directive::from_str("rattler_installs_packages=info").unwrap());
+
+    if verbose {
+        result = result.add_directive(Directive::from_str("resolvo=info").unwrap());
+    }
+
+    result
+}
